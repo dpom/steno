1f4f5f6122e4264cd5b2900c0228655de4164560
Dan Pomohaci <dan.pomohaci@gmail.com>
2018-08-27 14:13:44 +0300

    set project layout

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject steno "0.1.0-dev01"
+  :description "Digitize stenographic writings"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [eye-boof "2.1.0"]])
