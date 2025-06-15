048ea26d1b1021b8593afeec23f0e90d3095c5f4
Dan Pomohaci <dan.pomohaci@gmail.com>
2025-06-15 21:47:38 +0300

    [15] Update ent to new syntax

--- .ent.el
+++ .ent.el
@@ -9,21 +9,17 @@
 (setq ent-project-name "cljproj")
 (setq ent-clean-regexp "~$\\|\\.tex$")
 
-(require 'ent)
 
-(ent-tasks-init)
+(task :style '() "check code" "bb style")
 
-(task 'style '() "check code" '(lambda (&optional x) "bb style"))
+(task :format '() "format code" "bb format")
 
-(task 'format '() "format code" '(lambda (&optional x) "bb format"))
+(task :kondo '() "lint with kondo" "bb kondo")
 
-(task 'kondo '() "lint with kondo" '(lambda (&optional x) "bb kondo"))
+(task :libupdate '() "search for new libs versions" "bb libupdate" )
 
-(task 'libupdate '() "search for new libs versions" '(lambda (&optional x) "bb libupdate" ))
+(task :tests '() "run tests" "bb test")
 
-(task 'tests '() "run tests" '(lambda (&optional x) "bb test"))
-
-(task 'readme '() "build readme file" '(lambda (&optional x) "pandoc -o README.md tmp/README.org"))
 
 (provide '.ent)
 ;;; .ent.el ends here
