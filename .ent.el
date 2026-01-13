3d487a42463d5f3384e88a5b1c75a2536829422e
Dan Pomohaci <dan.pomohaci@gmail.com>
2026-01-13 06:21:45 +0200

    [31] Update ent syntax and fix format settings

--- .ent.el
+++ .ent.el
@@ -6,21 +6,31 @@
 
 ;; project settings
 (setq ent-project-home (file-name-directory (if load-file-name load-file-name buffer-file-name)))
-(setq ent-project-name "cljproj")
-(setq ent-clean-regexp "~$")
+(setq ent-project-name "steno")
+(setq ent-clean-regexp ".*~$\\|.*sync-conflict.*$")
 (setq ent-dirclean-regexp "__pycache__" )
 
+(ent-load-default-tasks)
 
+(task "style"
+      :doc "Check code"
+      :action "bb style")
 
-(task :style '() "check code" "bb style")
+(task "format"
+      :doc "Format code"
+      :action "bb format")
 
-(task :format '() "format code" "bb format")
+(task "kondo"
+      :doc "Lint with kondo"
+      :action "bb kondo")
 
-(task :kondo '() "lint with kondo" "bb kondo")
+(task "libupdate"
+      :doc "Search for new libs versions"
+      :action "bb libupdate" )
 
-(task :libupdate '() "search for new libs versions" "bb libupdate" )
-
-(task :tests '() "run tests" "bb test")
+(task "tests"
+      :doc "Run tests"
+      :action "bb test")
 
 
 (provide '.ent)
